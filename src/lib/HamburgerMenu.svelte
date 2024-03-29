58b80c3a48e6bbb1d71252a10ffe680180b36d3d
w3cdp6084-dev <yusukew3@gmail.com>
2024-03-30 08:41:33 +0900

    メニュー調整中

--- src/lib/HamburgerMenu.svelte
+++ src/lib/HamburgerMenu.svelte
@@ -2,22 +2,36 @@
   import { gsap } from 'gsap';
   let button;
 
-  function animateButton() {
-    const tl = gsap.timeline();
-    tl.to(button, { width: 500, duration: 1, ease: "power1.inOut" })
-      .to(button, { height: 500, duration: 1, ease: "power1.inOut" });
+  let isOpen = false;
+
+  function toggleNav() {
+    if (!isOpen) {
+      // ボタンを水平に400px、その後に垂直に300px伸ばす
+      gsap.to(button, { width: 400, duration: 0.5, ease: "power1.inOut" })
+        .then(() => {
+          gsap.to(button, { height: 300, duration: 0.5, ease: "power1.inOut" });
+        });
+    } else {
+      // ボタンを垂直に元のサイズに戻し、その後に水平に戻す
+      gsap.to(button, { height: 50, duration: 0.5, ease: "power1.inOut" })
+        .then(() => {
+          gsap.to(button, { width: 100, duration: 0.5, ease: "power1.inOut" });
+        });
+    }
+    isOpen = !isOpen;
   }
 </script>
 
-<button bind:this={button} on:click={animateButton} class="animate-button">
-  Click Me
+<button bind:this={button} on:click={toggleNav} class="nav-button">
+  Menu
 </button>
 
 <style>
-  .animate-button {
+  .nav-button {
     width: 100px; /* 初期幅 */
     height: 50px; /* 初期高さ */
     transition: all 0.5s ease-in-out; /* CSSのトランジションを削除 */
-    overflow: hidden; /* テキストがはみ出さないように */
+    margin: 0 auto;
+    display: block;
   }
 </style>
