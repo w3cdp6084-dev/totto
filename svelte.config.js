178379def68c7ebcd0d271c34e70d854f041f4e4
w3cdp6084-dev <yusukew3@gmail.com>
2024-04-10 10:10:54 +0900

    sass設定

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import adapter from '@sveltejs/adapter-vercel';
-
+import sveltePreprocess from 'svelte-preprocess';
 export default {
+	preprocess: sveltePreprocess(),
 	kit: {
 		adapter: adapter({
 			// see below for options that can be set here
