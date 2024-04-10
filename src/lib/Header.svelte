7fe2baeef9d2e182a882b64840e7e46e6cfdb2fd
w3cdp6084-dev <yusukew3@gmail.com>
2024-04-11 08:31:04 +0900

    Dark Mode仮実装

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+ import DarkMode from './DarkMode.svelte';
 	import HamburgerMenu from './HamburgerMenu.svelte';
 </script>
 
@@ -7,6 +8,7 @@
 	<div class="header-container">
 		<img src="/images/logo.svg" alt="logo" class="logo" />
 	</div>
+	<DarkMode />
 </header>
 
 <style lang="scss">
